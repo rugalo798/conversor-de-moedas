4bf6bd5a68d897c48d674bf725bc54aaa254a1e7
rugalo798 <34358578+rugalo798@users.noreply.github.com>
2020-06-24 05:08:05 -0300

    Arrumando o codigo
    arrumei algumas coisas no codigo deixando ele um pouco mais limpo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import './App.css'
-import Conversor from './components/Conversor.js'
+import React, { Component } from "react";
+import "./App.css";
+import Conversor from "./components/Conversor.js";
 
 class App extends Component {
- render(){
-  return (
-    <div>
-      <h1>Conversor de moedas</h1>
-      <div className="linha">
-      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
-      <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+  render() {
+    return (
+      <div>
+        <h1>Conversor de moedas</h1>
+        <div className="linha">
+          <Conversor moedaA="USD" moedaB="BRL" />
+          <Conversor moedaA="BRL" moedaB="USD" />
+        </div>
+        <div className="linha">
+          <Conversor moedaA="CAD" moedaB="BRL" />
+          <Conversor moedaA="BRL" moedaB="CAD" />
+        </div>
+        <div className="linha">
+          <Conversor moedaA="EUR" moedaB="BRL" />
+          <Conversor moedaA="BRL" moedaB="EUR" />
+        </div>
       </div>
-      <div className="linha">
-      <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
-      <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
-      </div>
-      <div className="linha">
-      <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
-      <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
-      </div>
-    </div>
-  );
- }
+    );
+  }
 }
 
 export default App;
